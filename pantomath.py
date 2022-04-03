ff38509d554b043495e8b759de54969ed883d883
Randy C. Will <randall.will@gmail.com>
2022-04-03 14:37:49 -0700

    Add some accelerators

--- pantomath.py
+++ pantomath.py
@@ -30,10 +30,12 @@ class MainFrame(wx.Frame):
         scan_fronts = scan_menu.AppendRadioItem(wx.ID_ANY, "Only &Fronts", "Scan backs of all pages using hardware duplexer")
         scan_backs = scan_menu.AppendRadioItem(wx.ID_ANY, "Only &Backs", "Scan fronts of all pages")
         fileMenu.AppendSeparator()
-        scan_all_from_adf = scan_menu.Append(wx.ID_ANY, "Scan All from &ADF", "Scan all pages from ADF")
+        scan_all_from_adf = scan_menu.Append(wx.ID_ANY, "Scan All from &ADF\tALT-A", "Scan all pages from ADF")
         self.Bind(wx.EVT_MENU, self.ScanAllFromADF, scan_all_from_adf)
-        scan_one_from_flatbed = scan_menu.Append(wx.ID_ANY, "Scan &One Page from Flatbed", "Scan a single page from the flatbed")
-        scan_multiple_from_flatbed = scan_menu.Append(wx.ID_ANY, "Scan Multiple &Pages from Flatbed", "Scan multiple pages from the flatbed glass, one at a time with a confirmation dialog in between each")
+        scan_one_from_flatbed = scan_menu.Append(wx.ID_ANY, "Scan &One Page from Flatbed\tALT-O", "Scan a single page from the flatbed")
+        self.Bind(wx.EVT_MENU, self.ScanOneFromFlatbed, scan_one_from_flatbed)
+        scan_multiple_from_flatbed = scan_menu.Append(wx.ID_ANY, "Scan Multiple &Pages from Flatbed\tALT-P", "Scan multiple pages from the flatbed glass, one at a time with a confirmation dialog in between each")
+        self.Bind(wx.EVT_MENU, self.ScanMultipleFromFlatbed, scan_multiple_from_flatbed)
 
         helpMenu = wx.Menu()
         aboutItem = helpMenu.Append(wx.ID_ABOUT)
@@ -48,6 +50,13 @@ class MainFrame(wx.Frame):
 
     def ScanAllFromADF(self, event):
         wx.MessageBox("Scan all from ADF is not yet implemented")
+
+    def ScanOneFromFlatbed(self, event):
+        wx.MessageBox("Scan one from Flatbed is not yet implemented")
+
+    def ScanMultipleFromFlatbed(self, event):
+        wx.MessageBox("Scan multiple from Flatbed is not yet implemented")
+
 
     def Exit(self, event):
         self.Close(True)
