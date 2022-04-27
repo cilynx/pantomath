17f1224b8079b9bf2a37efded73639051b14a2d6
Randy C. Will <randall.will@gmail.com>
2022-04-27 13:44:24 -0700

    Support for originals from path string

--- src/original.py
+++ src/original.py
@@ -1,9 +1,23 @@
+import os
+import shutil
+import filetype
+import pdf2image
+
 from .image import Image
 
 
 class Original():
     def __init__(self, pages):
-        if isinstance(pages[0], Image):
+        self.original_path = None
+        if isinstance(pages, str):
+            if os.path.isfile(pages):
+                self.original_path = pages
+                kind = filetype.guess(pages)
+                if not kind:
+                    raise ValueError("Unknown filetype.")
+                elif kind.mime.endswith('/pdf'):
+                    self.pages = [Image(page) for page in pdf2image.convert_from_path(pages)]
+        elif isinstance(pages[0], Image):
             self.pages = pages
         else:
             self.pages = [Image(page) for page in pages]
@@ -17,6 +31,11 @@ class Original():
 
     def save(self, filepath):
         print(f'Original.save({filepath})')
+        if self.original_path:
+            ext = os.path.splitext(self.original_path)[1]
+            dir = os.path.dirname(filepath)
+            dest = os.path.join(dir, 'original.' + ext)
+            shutil.move(self.original_path, dest)
         if isinstance(self.pages[0], Image):
             print(f'Original.save(): Saving {self.pages}')
             self.pages[0].save(filepath,
